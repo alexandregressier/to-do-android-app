e09dfbc64641ed65e9e027e613ca9e0216d4f694
Alexandre Gressier <alexandre@gressier.dev>
2021-10-14 12:28:44 +0200

    Define the Task DAO

---
+++ app/src/main/java/dev/gressier/todo/data/TaskDao.kt
@@ -0,0 +1,52 @@
+package dev.gressier.todo.data
+
+import androidx.room.*
+import androidx.room.OnConflictStrategy.IGNORE
+import dev.gressier.todo.data.models.Task
+import dev.gressier.todo.data.models.TaskId
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+
+    @Query("SELECT * FROM task WHERE id = :id")
+    fun getTask(id: TaskId): Flow<Task?>
+
+    @Query("SELECT * FROM task ORDER BY id ASC")
+    fun getAllTasks(): Flow<List<Task>>
+
+    @Query("""
+        SELECT * FROM task
+        ORDER BY CASE
+            WHEN priority = 'LOW' THEN 1
+            WHEN priority = 'MEDIUM' THEN 2
+            WHEN priority = 'HIGH' THEN 3
+        END
+    """)
+    fun getTasksByLowestPriority(): Flow<List<Task>>
+
+    @Query("""
+        SELECT * FROM task
+        ORDER BY CASE
+            WHEN priority = 'HIGH' THEN 1
+            WHEN priority = 'MEDIUM' THEN 2
+            WHEN priority = 'LOW' THEN 3
+        END
+    """)
+    fun getTasksByHighestPriority(): Flow<List<Task>>
+
+    @Query("SELECT * FROM task WHERE title LIKE :text OR description LIKE :text")
+    fun searchTasks(text: String): Flow<List<Task>>
+
+    @Insert(onConflict = IGNORE)
+    suspend fun addTask(task: Task): TaskId
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Delete
+    suspend fun deleteAllTasks()
+}
