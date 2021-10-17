ce9651b13d6327837aa5cb419e5744042456beb2
Alexandre Gressier <alexandre@gressier.dev>
2021-10-17 23:12:28 +0200

    Create an extension function for toast

--- app/src/main/java/dev/gressier/todo/util/Extensions.kt
+++ app/src/main/java/dev/gressier/todo/util/Extensions.kt
@@ -1,6 +1,12 @@
 package dev.gressier.todo.util
 
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
 import java.util.*
 
 fun String.capitalize(): String =
     lowercase().replaceFirstChar { it.titlecase(Locale.getDefault()) }
+
+fun Context.toast(@StringRes resId: Int) =
+    Toast.makeText(this, getString(resId), Toast.LENGTH_SHORT).show()
