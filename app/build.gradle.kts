f9cf4ffeb0a944b982049d072bc00fcd7201c7ff
Alexandre Gressier <alexandre@gressier.dev>
2021-10-18 21:22:48 +0200

    Upgrade minimum SDK version to 26 (Android 8.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.gressier.todo"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 31
         versionCode = 1
         versionName = "1.0"
