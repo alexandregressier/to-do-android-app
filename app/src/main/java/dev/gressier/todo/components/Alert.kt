85b3948c2ee31b676fa57c2a982b246ec87a75a9
Alexandre Gressier <alexandre@gressier.dev>
2021-10-20 00:37:47 +0200

    Define the Alert composable

---
+++ app/src/main/java/dev/gressier/todo/components/Alert.kt
@@ -0,0 +1,44 @@
+package dev.gressier.todo.components
+
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import dev.gressier.todo.R
+
+@Composable
+fun Alert(
+    isPresented: Boolean,
+    title: String,
+    message: String,
+    onYesClick: () -> Unit,
+    onNoClick: () -> Unit,
+) {
+    if (isPresented)
+        AlertDialog(
+            onDismissRequest = { onNoClick() },
+            title = {
+                Text(
+                    title,
+                    fontSize = MaterialTheme.typography.h5.fontSize,
+                    fontWeight = FontWeight.Bold,
+                )
+            },
+            text = {
+                Text(
+                    message,
+                    fontSize = MaterialTheme.typography.subtitle1.fontSize,
+                    fontWeight = FontWeight.Normal,
+                )
+            },
+            confirmButton = { Button({ onYesClick(); onNoClick() }) {
+                Text(stringResource(R.string.alert_yes_button)) }
+            },
+            dismissButton = { Button({ onNoClick() }) {
+                Text(stringResource(R.string.alert_no_button)) }
+            },
+        )
+}
