c9697e197eeb85a9632cdfd4097525d35cb80443
Alexandre Gressier <alexandre@gressier.dev>
2021-10-20 16:47:47 +0200

    Animate the logo of the splash screen

--- app/src/main/java/dev/gressier/todo/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/dev/gressier/todo/ui/screens/splash/SplashScreen.kt
@@ -1,19 +1,24 @@
 package dev.gressier.todo.ui.screens.splash
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.gressier.todo.R
 import dev.gressier.todo.ui.theme.ToDoTheme
 import dev.gressier.todo.ui.theme.splashScreenBackgroundColor
@@ -25,11 +30,29 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     navigateToTaskListScreen: () -> Unit = {},
 ) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val offsetState by animateDpAsState(
+        if (startAnimation) 0.dp else splashScreenLogoHeight,
+        tween((Config.SPLASH_SCREEN_DURATION * 0.8).toInt())
+    )
+    val alphasState by animateFloatAsState(
+        if (startAnimation) 1f else 0f,
+        tween((Config.SPLASH_SCREEN_DURATION * 0.8).toInt())
+    )
+
     LaunchedEffect(Unit) {
+        startAnimation = true
         delay(Config.SPLASH_SCREEN_DURATION)
         navigateToTaskListScreen()
     }
-    Box(Modifier.fillMaxSize().background(MaterialTheme.colors.splashScreenBackgroundColor), Alignment.Center) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.splashScreenBackgroundColor)
+            .offset(y = offsetState)
+            .alpha(alphasState),
+        Alignment.Center,
+    ) {
        Image(
            painterResource(R.drawable.ic_logo_light.takeUnless { isSystemInDarkTheme() } ?: R.drawable.ic_logo_dark),
            stringResource(R.string.description_todo_app_logo),
